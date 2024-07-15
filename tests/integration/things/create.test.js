18c093f62eb84f7446d70000d404d8545a914a70
Nathan-ODS <nathan.oliveira.da.silva.pro@gmail.com>
2024-07-15 09:57:34 +0200

    adding jest

--- tests/integration/things/create.test.js
+++ tests/integration/things/create.test.js
@@ -1,7 +1,3 @@
-import test from 'node:test'
-import assert from 'node:assert';
-import app from '../../../src/index'
-
-test('truthiness of true', () => {
-  assert.strictEqual(true, true)
+test('oui', () => {
+  expect(true).toBeTruthy;
 })
