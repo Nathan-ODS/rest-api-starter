ae1de096ce4791b73d97f08626f6092cc89214b4
Nathan-ODS <nathan.oliveira.da.silva.pro@gmail.com>
2025-01-15 23:24:06 +0100

    format

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import v1ThingsRouter from './v1/routes/thingRoutes.js';
+import express from "express";
+import bodyParser from "body-parser";
+import v1ThingsRouter from "./v1/routes/thingRoutes.js";
 
 const app = express();
 
 // uses body-parser to parse incoming JSON data from requests
 app.use(bodyParser.json());
-app.use('/api/v1/things', v1ThingsRouter);
+app.use("/api/v1/things", v1ThingsRouter);
+
+app.listen(3000, () => {
+  console.log("api running on http://localhost:3000");
+});
 
 export default app;
